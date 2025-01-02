fd3f17cfec770d570783d6b0d94615337f9b4022
Igor Eremin <eremin.igor@icloud.com>
2025-01-01 22:51:31 -0800

    fix: returning back to working version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    VITE_APP_API_BASE_URL: process.env.VITE_APP_API_BASE_URL,
-  },
 })
