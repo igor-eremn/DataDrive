494198c9789652dc0c796a9e9c4d0282a65c138d
Igor Eremin <eremin.igor@icloud.com>
2025-01-01 22:47:10 -0800

    fix: vercel pls see the env variable!!!

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
     VITE_APP_API_BASE_URL: process.env.VITE_APP_API_BASE_URL,
   },
 })
